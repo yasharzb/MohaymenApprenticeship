62c44c623bedbb893609bfa8a12d787720f13747
yasharzb <yasharzb@mail.ce.sharif.edu>
2019-07-16 16:31:53 +0430

    Initializer uses DB.

--- src/Models/Initializer.java
+++ src/Models/Initializer.java
@@ -8,6 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class Initializer {
@@ -25,15 +26,21 @@ public class Initializer {
         return INITIALIZER;
     }
 
-    public void main() {
+    public void main(Connection connection) {
         try {
             inputStream = new URL(SOURCE).openStream();
         } catch (IOException e) {
             e.printStackTrace();
         }
         rss = new RSS(getURLContent(inputStream));
-        rss.extractNews();
-        save();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into rss." +
+                    "newsIndes values (?,?,?,?);");
+            rss.extractNews(preparedStatement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+//        save();
     }
 
     public void save() {
@@ -51,10 +58,17 @@ public class Initializer {
         }
     }
 
-    public void initializeRSS() {
+    public void initializeRSS(Statement statement) {
         try {
-            rss = RSS.fromJson(new BufferedReader(new FileReader("./src/Data/rss.json")).readLine());
-        } catch (IOException e) {
+            //rss = RSS.fromJson(new BufferedReader(new FileReader("./src/Data/rss.json")).readLine());
+            ResultSet resultSet = statement.executeQuery("select * from rss.newsIndes;");
+            rss = new RSS();
+            while (resultSet.next()) {
+                News news = new News(resultSet.getInt(1), resultSet.getString(2),
+                        resultSet.getString(3), resultSet.getInt(4));
+                rss.getItems().put(news.getId(), news);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -75,5 +89,15 @@ public class Initializer {
 
     public RSS getRss() {
         return rss;
+    }
+
+    public void incrementNewsView(Statement statement, News news) {
+        news.incrementView();
+        try {
+            statement.executeUpdate("update rss.newsIndes set View = " + news.getViews() +
+                    " WHERE Id = " + news.getId() + ";");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
