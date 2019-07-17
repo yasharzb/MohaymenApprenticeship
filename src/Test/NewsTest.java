b0c59caa1ec1460c2002aea4623695e0ab573c09
yasharzb <yasharzb@mail.ce.sharif.edu>
2019-07-17 14:27:22 +0430

    NewsTest is ready.

--- src/Test/NewsTest.java
+++ src/Test/NewsTest.java
@@ -1,18 +1,25 @@
 package Test;
 
 import Models.News;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class NewsTest {
-    @Before
-    public void initiation(){
+    private News news;
 
+    @Before
+    public void initiation() {
+        news = new News();
     }
 
     @Test
     public void incrementView() {
+        final int expected = news.getViews();
+        news.incrementView();
+        final int actual = news.getViews();
+        Assert.assertEquals(actual, expected);
     }
 }
